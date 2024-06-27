abb5c6515797fb6d2e5613dbfd163b30f311583e
kushal4345 <jeeultimate4345@gmail.com>
2024-06-27 17:01:15 +0530

    subjects

---
+++ resources.js
@@ -0,0 +1,15 @@
+// const element = document.getElementById('scroll-hide');
+// const cr = document.getElementById('copyright');
+// window.addEventListener('scroll', function() {
+// if(cr.style.color == 'white')
+//     {  
+//     if (window.scrollY > 10) {
+//         element.style.display = 'none';
+//     }
+// }
+
+// });
+// document.getElementById('click').addEventListener('click', function() {
+//     element.style.display = 'block';
+//     // You can call any function here
+// });
--- web.js
+++ web.js
@@ -1,10 +1,18 @@
 const meetingCodes = {
-    meetingCode1: 'abcd-efgh-ijkl',
-    meetingCode2: 'mnop-qrst-uvwx',
-    meetingCode3: 'yzab-cdef-ghij'
+    meetingCode1: 'tpr-msju-fra',
+    meetingCode2: 'vdd-dvup-csh',
+    meetingCode3: 'ako-wtqv-sso',
+    meetingCode4: 'ako-wtqv-sso',
+    meetingCode5: 'ako-wtqv-sso',
+    meetingCode6: 'ako-wtqv-sso',
+    meetingCode7: 'ako-wtqv-sso',
+    meetingCode8: 'ako-wtqv-sso',
+      meetingCode8: 'ako-wtqv-sso'
+
+
 };
 
-function joinMeeting(meetingCodeKey) {
+function joinMeetingg(meetingCodeKey) {
     const meetingCode = meetingCodes[meetingCodeKey];
     const meetUrl = `https://meet.google.com/${meetingCode}`;
     window.open(meetUrl, '_blank');
