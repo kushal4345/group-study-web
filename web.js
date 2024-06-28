68e04f55f70adcfe089c4f93d0d74e37c2353f9a
Wiz Abhi <abhishekg8318@gmail.com>
2024-06-28 18:05:50 +0530

    removed

--- web.js
+++ web.js
@@ -4,14 +4,8 @@ const meetingCodes = {
     meetingCode3: 'ako-wtqv-sso',
     meetingCode4: 'hzr-tzwe-obh',
     meetingCode5: 'ttp-fpbe-yne',
-    meetingCode6: 'urw-aetb-uux',
-    meetingCode7: 'vym-unqp-dgx',
-    meetingCode8: 'eam-tmhj-dfp',
-    meetingCode8: 'sxh-hhwn-sen'
-
-
+    meetingCode6: 'urw-aetb-uux'
 };
-
 function joinMeetingg(meetingCodeKey) {
     const meetingCode = meetingCodes[meetingCodeKey];
     const meetUrl = `https://meet.google.com/${meetingCode}`;
